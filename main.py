c351944fa9111cf8e03795760adc5e947cae57c0
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-14 16:53:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Orde Reaction Calculator</h1>", unsafe_allow_html=True)
 
+if "show_form" not in st.session_state:
+    st.session_state.show_form = False
+
+st.title("Manajemen Data")
+
 if st.button("Tambahkan Data"):
     st.session_state.show_form = not st.session_state.show_form
     
