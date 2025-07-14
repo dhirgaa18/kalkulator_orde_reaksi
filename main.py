861508b0c0c9162d66b67b9aa01f56ec0468d196
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-14 16:22:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'>Judul di Tengah</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Orde Reaction Calc</h1>", unsafe_allow_html=True)
