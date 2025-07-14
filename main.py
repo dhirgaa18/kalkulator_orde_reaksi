fa0145023a4a834def9529c93e19a4dd2555c5bc
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-14 16:47:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Orde Reaction Calculator</h1>", unsafe_allow_html=True)
+
+st.button("Tambahkan Data", type="primary")
+if st.button("Anjay"):
+  
