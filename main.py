1c1f58e4475f27305ec805b7c3ebce4cfa73794e
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-14 16:51:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,5 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Orde Reaction Calculator</h1>", unsafe_allow_html=True)
 
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
-
-if st.button("Aloha", type="tertiary"):
-    st.write("Ciao")
+if st.button("Tambahkan Data"):
+    st.session_state.show_form = not st.session_state.show_form
