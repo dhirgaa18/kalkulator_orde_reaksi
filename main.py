6c2670c57059c864fbc130f3892f29fa010cbb85
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-18 10:51:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Kinetika Reaksi", layout="wide")
 
 # Sidebar Navigasi
 st.sidebar.title("📂 Navigasi")
-page = st.sidebar.radio("Pilih Halaman", ["Beranda", "Analisis Orde", "Penentuan Orde", "Grafik Regresi", "Petunjuk", "Hasil"])
+page = st.sidebar.radio("Pilih Halaman", ["Beranda", "Analisis Orde", "Penentuan Orde", "Petunjuk"])
 
 # ================================
 # 📌 BERANDA
